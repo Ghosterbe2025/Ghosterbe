78b56c0c423cb0fe0114de6b0e48ca3bc3246433
Ghosterbe2025 <ghosterbe@gmx.de>
2025-05-25 17:43:59 +0200

    1

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ def homepage():
 def admin_login():
     return render_template("admin_login.html")
 
+@app.route("/agb.html")
+def agb():
+    return render_template("agb.html")
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
